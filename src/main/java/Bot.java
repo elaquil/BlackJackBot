3c1acdbb861253e568c7ab63a4a6f69d0fd27074
Ismael <sonicismael2@gmail.com>
2021-05-27 17:25:30 +0930

    Hidden Token!

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -5,7 +5,7 @@ import javax.security.auth.login.LoginException;
 public class Bot {
 
     public static void main (String[] args) throws LoginException {
-        JDA jda = JDABuilder.createDefault("Nzk1MjM1ODg4MjkyMDM2NjI4.X_GbIg.KroQD02brg6Lz373yArCrS1oHos").build();
+        JDA jda = JDABuilder.createDefault(System.getenv("TOKEN")).build();
         jda.addEventListener(new BotEvent());
     }
 
