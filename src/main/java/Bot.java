bc1cbc613542249b1b61408c6c6a1b6f4fced008
Ismael <sonicismael2@gmail.com>
2021-05-27 17:12:43 +0930

    Prep for Heroku deployment

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -5,7 +5,7 @@ import javax.security.auth.login.LoginException;
 public class Bot {
 
     public static void main (String[] args) throws LoginException {
-        JDA jda = JDABuilder.createDefault("Nzk1MjM1ODg4MjkyMDM2NjI4.X_GbIg.LHwg8JE_IJoLikwBfRBnY6h65hc").build();
+        JDA jda = JDABuilder.createDefault("Nzk1MjM1ODg4MjkyMDM2NjI4.X_GbIg.KroQD02brg6Lz373yArCrS1oHos").build();
         jda.addEventListener(new BotEvent());
     }
 
